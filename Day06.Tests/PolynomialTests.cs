54e6cac5db3cc266bad99222178203e34e620bb1
Nikita Demidenko <nikita.demidenko24@gmail.com>
2020-02-01 17:30:37 +0300

    feat: add implementation and unit tests for Polinomial task.

---
+++ Day06.Tests/PolynomialTests.cs
@@ -0,0 +1,168 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day06.Tests
+{
+    public class PolynomialTests
+    { 
+        public static IEnumerable<TestCaseData> TestCasesForAdd
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynomial(2, 5, 1), new Polynomial(3, -5, 1), new Polynomial(5, 0, 2));
+                yield return new TestCaseData(new Polynomial(34.1, 28.2, 13.322, 0, 4.5, 90.908), new Polynomial(10, 0, 2.5), new Polynomial(44.1, 28.2, 15.822, 0, 4.5, 90.908));
+            }
+        }
+
+        public static IEnumerable<TestCaseData> TestCasesForSubtract
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynomial(2, 5, 1), new Polynomial(3, -5, 1), new Polynomial(-1, 10, 0));
+                yield return new TestCaseData(new Polynomial(34.1, 28.2, 13.322, 0, 4.5, 90.908), new Polynomial(10, 0, 2.5), new Polynomial(24.1, 28.2, 10.822, 0, 4.5, 90.908));
+            }
+        }
+
+        public static IEnumerable<TestCaseData> TestCasesForMultiply
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynomial(34.1, 28.2, 13.322, 0, 4.5, 90.908), new Polynomial(10, 0, 2.5), new Polynomial(341, 282, 218.47, 70.5, 78.305, 909.08, 11.25, 227.27));
+                yield return new TestCaseData(new Polynomial(2.2, 2, 0, 1.5, 15, 128.311), new Polynomial(0), new Polynomial(0, 0, 0, 0, 0, 0));
+            }
+        }
+
+        public static IEnumerable<TestCaseData> TestCasesForDivide
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynomial(34.1, 28.2, 13.322, 0, 4.5, 90.908), 2.0, new Polynomial(17.05, 14.1, 6.661, 0, 2.25, 45.454));
+                yield return new TestCaseData(new Polynomial(2.2, 2, 0, 1.5, 15, 128.311), 1.0, new Polynomial(2.2, 2, 0, 1.5, 15, 128.311));
+            }
+        }
+
+        public static IEnumerable<TestCaseData> TestCasesForEquals
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynomial(34.1, 28.2, 13.322, 0, 4.5, 90.908), new Polynomial(34.1, 28.2, 13.322, 0, 4.5, 90.908));
+                yield return new TestCaseData(new Polynomial(341, 282, 218.47, 70.5, 78.305, 909.08, 11.25, 227.27), new Polynomial(341, 282, 218.47, 70.5, 78.305, 909.08, 11.25, 227.27));
+            }
+        }
+
+        public static IEnumerable<TestCaseData> TestCasesForNotEquals
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynomial(34.1, 28.2, 13.322, 0.01, 4.5, 90.908), new Polynomial(34.1, 28.2, 13.322, 0, 4.5, 90.908));
+                yield return new TestCaseData(new Polynomial(341, 282, 218.47, 70.5), new Polynomial(341, 282, 218.47, 70.5, 78.305, 909.08, 11.25, 227.27));
+            }
+        }
+
+        public static IEnumerable<TestCaseData> TestCasesForToString
+        {
+            get
+            {
+                yield return new TestCaseData(new Polynomial(34.1, 28.2, 13.322, 0.01, 4.5, 90.908), "90.908x^5+4.5x^4+0.01x^3+13.322x^2+28.2x+34.1");
+                yield return new TestCaseData(new Polynomial(6, -8, 4), "4x^2-8x+6");
+                yield return new TestCaseData(new Polynomial(-23.4), "-23.4");
+                yield return new TestCaseData(new Polynomial(0, -1.1, 23.4, -5.5), "-5.5x^3+23.4x^2-1.1x");
+            }
+        }
+
+        [Test]
+        public void OperatorDivision_ByZero_ThrowDivideByZeroException()
+        {
+            var test = new Polynomial(2.4, 12, 1.1, 4, -34.4);
+            Assert.Throws<DivideByZeroException>(() => Polynomial.Divide(test, 0.00000000000001));
+        }
+
+        [TestCaseSource(nameof(TestCasesForEquals))]
+        public void EqualsTests(Polynomial left, Polynomial right)
+        {
+            Assert.That(left == right);
+        }
+
+        [TestCaseSource(nameof(TestCasesForNotEquals))]
+        public void NotEqualsTests(Polynomial left, Polynomial right)
+        {
+            Assert.That(left != right);
+        }
+
+        [TestCaseSource(nameof(TestCasesForEquals))]
+        public void GetHashCodeTests_Equal(Polynomial left, Polynomial right)
+        {
+            Assert.AreEqual(left.GetHashCode(), right.GetHashCode());
+        }
+
+        [TestCaseSource(nameof(TestCasesForNotEquals))]
+        public void GetHashCodeTests_NotEqual(Polynomial left, Polynomial right)
+        {
+            Assert.That(left.GetHashCode() != right.GetHashCode());
+        }
+
+        [TestCaseSource(nameof(TestCasesForAdd))]
+        public void AddOperatorTests(Polynomial left, Polynomial right, Polynomial expected)
+        {
+            var actual = Polynomial.Add(left, right);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCaseSource(nameof(TestCasesForSubtract))]
+        public void SubtractOperatorTests(Polynomial left, Polynomial right, Polynomial expected)
+        {
+            var actual = Polynomial.Subtract(left, right);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCaseSource(nameof(TestCasesForMultiply))]
+        public void MultiplyOperatorTests(Polynomial left, Polynomial right, Polynomial expected)
+        {
+            var actual = Polynomial.Multiply(left, right);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCaseSource(nameof(TestCasesForDivide))]
+        public void DivideOperatorTests(Polynomial left, double right, Polynomial expected)
+        {
+            var actual = Polynomial.Divide(left, right);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(ExpectedResult = true)]
+        public bool OperatorPlus_WithNumber()
+        {
+            Polynomial test = new Polynomial(0, 1, 2, 3, 4);
+            var expected = new Polynomial(5.5, 1, 2, 3, 4);
+            var actual = 5.5 + test;
+            return expected.Equals(actual);
+        }
+
+        [TestCase(ExpectedResult = true)]
+        public bool OperatorMinus_WithNumber()
+        {
+            Polynomial test = new Polynomial(4, 1, 2, 3, 4);
+            var expected = new Polynomial(-2, -1, -2, -3, -4);
+            var actual = 2 - test;
+            return expected.Equals(actual);
+        }
+
+        [TestCase(ExpectedResult = true)]
+        public bool OperatorMultiply_WithNumber()
+        {
+            Polynomial test = new Polynomial(0, 1, 2, 3, 4);
+            var expected = new Polynomial(0, 2.5, 5, 7.5, 10);
+            var actual = 2.5 * test;
+            return expected.Equals(actual);
+        }
+
+        [TestCaseSource(nameof(TestCasesForToString))]
+        public void PolynomialToString(Polynomial test, string expected)
+        {
+            var actual = test.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Day06/Polynomial.cs
@@ -0,0 +1,408 @@
+﻿using System;
+using System.Globalization;
+
+namespace Day06
+{
+    public class Polynomial
+    {
+        public const double Epsilon = 0.0000000001;
+
+        private readonly double[] coefficients;
+
+        public Polynomial(params double[] coefficients)
+        {
+            if (coefficients == null)
+            {
+                throw new ArgumentNullException(nameof(coefficients));
+            }
+
+            this.coefficients = coefficients;
+        }
+
+        public static Polynomial operator +(Polynomial left, Polynomial right)
+        {
+            if (left is null)
+            {
+                throw new ArgumentNullException(nameof(left));
+            }
+
+            if (right is null)
+            {
+                throw new ArgumentNullException(nameof(right));
+            }
+
+            int leftLength = left.coefficients.Length;
+            int rightLength = right.coefficients.Length;
+            double[] coefficients;
+            if (leftLength >= rightLength)
+            {
+                coefficients = new double[leftLength];
+                for (int i = 0; i < coefficients.Length; i++)
+                {
+                    coefficients[i] = left.coefficients[i];
+                }
+
+                for (int i = 0; i < rightLength; i++)
+                {
+                    coefficients[i] = left.coefficients[i] + right.coefficients[i];
+                }
+            }
+            else
+            {
+                coefficients = new double[rightLength];
+                for (int i = 0; i < coefficients.Length; i++)
+                {
+                    coefficients[i] = right.coefficients[i];
+                }
+
+                for (int i = 0; i < leftLength; i++)
+                {
+                    coefficients[i] = right.coefficients[i] + left.coefficients[i];
+                }
+            }
+
+            return new Polynomial(coefficients);
+        }
+
+        public static Polynomial operator +(Polynomial left, double right)
+        {
+            if (left is null)
+            {
+                throw new ArgumentNullException(nameof(left));
+            }
+
+            double[] coefficients = new double[left.coefficients.Length];
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                coefficients[i] = left.coefficients[i];
+            }
+
+            coefficients[0] += right;
+            return new Polynomial(coefficients);
+        }
+
+        public static Polynomial operator +(double left, Polynomial right)
+        {
+            if (right is null)
+            {
+                throw new ArgumentNullException(nameof(right));
+            }
+
+            return right + left;
+        }
+
+        public static Polynomial operator -(Polynomial left, Polynomial right)
+        {
+            if (left is null)
+            {
+                throw new ArgumentNullException(nameof(left));
+            }
+
+            if (right is null)
+            {
+                throw new ArgumentNullException(nameof(right));
+            }
+
+            int leftLength = left.coefficients.Length;
+            int rightLength = right.coefficients.Length;
+            double[] coefficients;
+            if (leftLength >= rightLength)
+            {
+                coefficients = new double[leftLength];
+                for (int i = 0; i < coefficients.Length; i++)
+                {
+                    coefficients[i] = left.coefficients[i];
+                }
+
+                for (int i = 0; i < rightLength; i++)
+                {
+                    coefficients[i] = left.coefficients[i] - right.coefficients[i];
+                }
+            }
+            else
+            {
+                coefficients = new double[rightLength];
+                for (int i = 0; i < coefficients.Length; i++)
+                {
+                    coefficients[i] = right.coefficients[i];
+                }
+
+                for (int i = 0; i < leftLength; i++)
+                {
+                    coefficients[i] = right.coefficients[i] - left.coefficients[i];
+                }
+            }
+
+            return new Polynomial(coefficients);
+        }
+
+        public static Polynomial operator -(Polynomial left, double right)
+        {
+            if (left is null)
+            {
+                throw new ArgumentNullException(nameof(left));
+            }
+
+            double[] coefficients = new double[left.coefficients.Length];
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                coefficients[i] = left.coefficients[i];
+            }
+
+            coefficients[0] -= right;
+
+            return new Polynomial(coefficients);
+        }
+
+        public static Polynomial operator -(double left, Polynomial right)
+        {
+            if (right is null)
+            {
+                throw new ArgumentNullException(nameof(right));
+            }
+
+            double[] coefficients = new double[right.coefficients.Length];
+            for (int i = 1; i < coefficients.Length; i++)
+            {
+                coefficients[i] = -right.coefficients[i];
+            }
+
+            coefficients[0] = left - right.coefficients[0];
+            return new Polynomial(coefficients);
+        }
+
+        public static Polynomial operator *(Polynomial left, Polynomial right)
+        {
+            if (left is null)
+            {
+                throw new ArgumentNullException(nameof(left));
+            }
+
+            if (right is null)
+            {
+                throw new ArgumentNullException(nameof(right));
+            }
+
+            int power = left.coefficients.Length + right.coefficients.Length - 2;
+            double[] coefficients = new double[power + 1];
+
+            for (int i = 0; i < left.coefficients.Length; i++)
+            {
+                for (int j = 0; j < right.coefficients.Length; j++)
+                {
+                    coefficients[i + j] += left.coefficients[i] * right.coefficients[j];
+                }
+            }
+
+            return new Polynomial(coefficients);
+        }
+
+        public static Polynomial operator *(Polynomial left, double right)
+        {
+            if (left is null)
+            {
+                throw new ArgumentNullException(nameof(left));
+            }
+
+            if (Math.Abs(right - 0.0) < Epsilon)
+            {
+                return new Polynomial(0, 0.0);
+            }
+
+            double[] coefficients = new double[left.coefficients.Length];
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                coefficients[i] = left.coefficients[i] * right;
+            }
+
+            return new Polynomial(coefficients);
+        }
+
+        public static Polynomial operator *(double left, Polynomial right)
+        {
+            if (right is null)
+            {
+                throw new ArgumentNullException(nameof(right));
+            }
+
+            return right * left;
+        }
+
+        public static Polynomial operator /(Polynomial left, double right)
+        {
+            if (left is null)
+            {
+                throw new ArgumentNullException(nameof(left));
+            }
+
+            if (Math.Abs(right - 0.0) < Epsilon)
+            {
+                throw new DivideByZeroException();
+            }
+
+            double[] coefficients = new double[left.coefficients.Length];
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                coefficients[i] = left.coefficients[i] / right;
+            }
+
+            return new Polynomial(coefficients);
+        }
+
+        public static bool operator ==(Polynomial left, Polynomial right)
+        {
+            return left is null ? false : left.Equals(right);
+        }
+
+        public static bool operator !=(Polynomial left, Polynomial right)
+        {
+            return !(left == right);
+        }
+
+        public static Polynomial Add(Polynomial left, Polynomial right)
+        {
+            return left + right;
+        }
+
+        public static Polynomial Add(Polynomial left, double number)
+        {
+            return left + number;
+        }
+
+        public static Polynomial Add(double number, Polynomial right)
+        {
+            return right + number;
+        }
+
+        public static Polynomial Subtract(Polynomial left, Polynomial right)
+        {
+            return left - right;
+        }
+
+        public static Polynomial Subtract(Polynomial left, double right)
+        {
+            return left - right;
+        }
+
+        public static Polynomial Subtract(double left, Polynomial right)
+        {
+            return left - right;
+        }
+
+        public static Polynomial Multiply(Polynomial left, Polynomial right)
+        {
+            return left * right;
+        }
+
+        public static Polynomial Multiply(Polynomial left, double right)
+        {
+            return left * right;
+        }
+
+        public static Polynomial Multiply(double left, Polynomial right)
+        {
+            return left * right;
+        }
+
+        public static Polynomial Divide(Polynomial left, double right)
+        {
+            return left / right;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Polynomial))
+            {
+                return false;
+            }
+
+            if (this.coefficients.Length != ((Polynomial)obj).coefficients.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < this.coefficients.Length; i++)
+            {
+                if (Math.Abs(this.coefficients[i] - ((Polynomial)obj).coefficients[i]) > Epsilon)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCodesSum = 0;
+            for (int i = 0; i < this.coefficients.Length; i++)
+            {
+                hashCodesSum += this.coefficients[i].GetHashCode();
+            }
+
+            return this.ShiftAndWrap(this.coefficients.Length.GetHashCode(), 2) ^ hashCodesSum.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            string result = string.Empty;
+            for (int i = this.coefficients.Length - 1; i >= 0; i--)
+            {
+                if (i == this.coefficients.Length - 1 && i > 1)
+                {
+                    result += this.coefficients[i].ToString(CultureInfo.InvariantCulture) + "x^" + i.ToString(CultureInfo.InvariantCulture);
+                }
+                else if (i == this.coefficients.Length - 1 && i == 1)
+                {
+                    result += this.coefficients[i].ToString(CultureInfo.InvariantCulture) + "x";
+                }
+                else if (i == this.coefficients.Length - 1 && i == 0)
+                {
+                    result += this.coefficients[i].ToString(CultureInfo.InvariantCulture);
+                }
+                else if (Math.Abs(this.coefficients[i] - 0.0) < Epsilon && this.coefficients.Length - 1 == 0)
+                {
+                    return "0";
+                }
+                else if (Math.Abs(this.coefficients[i] - 0.0) < Epsilon)
+                {
+                    continue;
+                }
+                else if (this.coefficients[i] > 0 && i > 1)
+                {
+                    result += "+" + this.coefficients[i].ToString(CultureInfo.InvariantCulture) + "x^" + i.ToString(CultureInfo.InvariantCulture);
+                }
+                else if (this.coefficients[i] > 0 && i == 1)
+                {
+                    result += "+" + this.coefficients[i].ToString(CultureInfo.InvariantCulture) + "x";
+                }
+                else if (this.coefficients[i] > 0 && i == 0)
+                {
+                    result += "+" + this.coefficients[i].ToString(CultureInfo.InvariantCulture);
+                }
+                else if (i == 1)
+                {
+                    result += this.coefficients[i].ToString(CultureInfo.InvariantCulture) + "x";
+                }
+                else if (i == 0)
+                {
+                    result += this.coefficients[i].ToString(CultureInfo.InvariantCulture);
+                }
+                else
+                {
+                    result += this.coefficients[i].ToString(CultureInfo.InvariantCulture) + "x^" + i.ToString(CultureInfo.InvariantCulture);
+                }
+            }
+
+            return result;
+        }
+
+        private int ShiftAndWrap(int value, int positions)
+        {
+            positions = positions & 0x1F;
+            uint number = BitConverter.ToUInt32(BitConverter.GetBytes(value), 0);
+            uint wrapped = number >> (32 - positions);
+            return BitConverter.ToInt32(BitConverter.GetBytes((number << positions) | wrapped), 0);
+        }
+    }
+}
